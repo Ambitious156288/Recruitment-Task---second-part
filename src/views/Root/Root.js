8c31092f55e894165127d0854ee26115dd784d50
Krystian Kalwik <ambitious156288@gmail.com>
2021-11-12 16:40:09 +0100

    global styles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import SearchingBar from 'components/SearchingBar';
 import Display from 'components/Display';
 
+const StyledWrapper = styled.div`
+  text-align: center;
+`;
+
 const Root = () => (
-  <>
-    <>
-      <SearchingBar />
-      <Display />
-    </>
-  </>
+  <StyledWrapper>
+    <SearchingBar />
+    <Display />
+  </StyledWrapper>
 );
 
 export default Root;
